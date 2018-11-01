a0522283727ddf2cf4d22012fe502ba6775056d0
tiget <tigetxd@gmail.com>
2018-11-01 21:28:39 +0300

    Added copy feature(in the code)

--- app/src/main/java/com/example/tiget/ontouchtest/MainActivity.java
+++ app/src/main/java/com/example/tiget/ontouchtest/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.tiget.ontouchtest;
 
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        final EditText editText = findViewById(R.id.editText);
+        final ImageView imageView = findViewById(R.id.CopyTextButton);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", editText.getText());
+                clipboard.setPrimaryClip(clip);
+            }
+        });
+
+*/
+
 
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout, new OnTouchFragment()).commit();
         }
+
 
     }
 
